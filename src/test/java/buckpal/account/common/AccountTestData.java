42489cc3799190510e722573cdf996440b05d980
LimJiNa <jn4624@gmail.com>
2024-04-07 23:19:08 +0900

    도메인 엔티티 단위 테스트

---
+++ src/test/java/buckpal/account/common/AccountTestData.java
@@ -0,0 +1,43 @@
+package buckpal.account.common;
+
+import buckpal.account.domain.Account;
+import buckpal.account.domain.ActivityWindow;
+import buckpal.account.domain.Money;
+
+import static buckpal.account.domain.Account.AccountId;
+
+public class AccountTestData {
+    public static AccountBuilder defaultAccount() {
+        return new AccountBuilder()
+                .withAccountId(new AccountId(42L))
+                .withBaselineBalance(Money.of(999L))
+                .withActivityWindow(new ActivityWindow(
+                        ActivityTestData.defaultActivity().build(),
+                        ActivityTestData.defaultActivity().build()));
+    }
+
+    public static class AccountBuilder {
+        private AccountId accountId;
+        private Money baselineBalance;
+        private ActivityWindow activityWindow;
+
+        public AccountBuilder withAccountId(AccountId accountId) {
+            this.accountId = accountId;
+            return this;
+        }
+
+        public AccountBuilder withBaselineBalance(Money baselineBalance) {
+            this.baselineBalance = baselineBalance;
+            return this;
+        }
+
+        public AccountBuilder withActivityWindow(ActivityWindow activityWindow) {
+            this.activityWindow = activityWindow;
+            return this;
+        }
+
+        public Account build() {
+            return Account.withId(this.accountId, this.baselineBalance, this.activityWindow);
+        }
+    }
+}
